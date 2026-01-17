a663bd4f06d4344eccf4795f3408ef3e01e6635d
Collin White <119827573+CollinJWhite@users.noreply.github.com>
2026-01-17 17:52:20 -0500

    Add files via upload

---
+++ HelloAutomation.py
@@ -0,0 +1,17 @@
+# this is the first comment
+spam = 1  # and this is the second comment
+          # ... and now a third!
+text = 'Hello World! I can write a quote: "Quothe the Raven, Nevermore." without a \\ '
+
+print(text + 
+      'because the text is wrapped in \'s. '
+'in addition, I can make line breaks in VS code without the need of a +.'
+      )
+
+helloText = "Hello"
+automationText = "Automation!"
+
+print(helloText[0] + automationText[7])
+print(helloText[-5] + automationText[-4])
+
+print(helloText + ' ' + automationText)
